556e5654aba468a448769c325b65fe6cbdbe0314
Sean Moon <ssamoon@ucla.edu>
2011-04-18 22:21:28 -0400

    first translate

--- translate.rb
+++ translate.rb
@@ -1,20 +1,36 @@
+# coding: utf-8
 require 'rubygems'
-require 'twitter'
 require 'yaml'
+require 'twitter'
+require 'nestful'
+
+GOOGLE_TRANSLATE_URL    = 'https://www.googleapis.com/language/translate/v2'
 
 credentials = YAML.load_file('credentials.yml')
-YOUR_CONSUMER_KEY       = credentials['YOUR_CONSUMER_KEY']
-YOUR_CONSUMER_SECRET    = credentials['YOUR_CONSUMER_SECRET']
-YOUR_OAUTH_TOKEN        = credentials['YOUR_OAUTH_TOKEN']
-YOUR_OAUTH_TOKEN_SECRET = credentials['YOUR_OAUTH_TOKEN_SECRET']
+
+GOOGLE_API_KEY             = credentials['GOOGLE_API_KEY']
+TWITTER_CONSUMER_KEY       = credentials['TWITTER_CONSUMER_KEY']
+TWITTER_CONSUMER_SECRET    = credentials['TWITTER_CONSUMER_SECRET']
+TWITTER_OAUTH_TOKEN        = credentials['TWITTER_OAUTH_TOKEN']
+TWITTER_OAUTH_TOKEN_SECRET = credentials['TWITTER_OAUTH_TOKEN_SECRET']
 
 Twitter.configure do |config|
-  config.consumer_key = YOUR_CONSUMER_KEY
-  config.consumer_secret = YOUR_CONSUMER_SECRET
-  config.oauth_token = YOUR_OAUTH_TOKEN
-  config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+  config.consumer_key = TWITTER_CONSUMER_KEY
+  config.consumer_secret = TWITTER_CONSUMER_SECRET
+  config.oauth_token = TWITTER_OAUTH_TOKEN
+  config.oauth_token_secret = TWITTER_OAUTH_TOKEN_SECRET
 end
 
-#Twitter.update("second post")
-puts Twitter.user_timeline("mingkki21").first.text
+tweet_in_korean = Twitter.user_timeline("mingkki21").first.text
 
+params = {
+  key: GOOGLE_API_KEY,
+  source: 'ko',
+  target: 'en',
+  q: tweet_in_korean
+}
+
+response = Nestful.get GOOGLE_TRANSLATE_URL, params: params
+json = JSON.parse(response)
+first_result = json['data']['translations'].first['translatedText']
+puts "#{tweet_in_korean} -> #{first_result}"
